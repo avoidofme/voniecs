ba01b0dd5119d5f80ec5cc093d1ca5852a49eea2
avoidofme <avoidofme@proton.me>
2026-06-26 15:02:30 +0700

    feat: base api 2

--- vonapi/main.go
+++ vonapi/main.go
@@ -1,20 +1,7 @@
 package main
 
-import (
-	"log"
-	"net/http"
-
-	"github.com/gin-gonic/gin"
-)
+import "voniecs/vonapi/von"
 
 func main() {
-	r := gin.Default()
-	r.GET("/", handleRoot)
-	if err := r.Run("localhost:8080"); err != nil {
-		log.Fatal("failed to run vonapi")
-	}
-}
-
-func handleRoot(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, gin.H{"message": "hellow orld"})
+	von.Start()
 }
---
+++ vonapi/von/von.go
@@ -0,0 +1,25 @@
+// Package von
+package von
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func Start() {
+	r := gin.Default()
+	v1 := r.Group("api/v1")
+	{
+		v1.GET("/hw", handleHw)
+	}
+
+	if err := r.Run("localhost:8080"); err != nil {
+		log.Fatal("fail to run vonapi")
+	}
+}
+
+func handleHw(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, gin.H{"message": "hellow orld"})
+}
